2db26d8307639f8a491d44aa79bdf43f37944169
William Silva <williamsilvaec@gmail.com>
2026-01-23 20:55:01 -0300

    feat: Configurar um banco de dados real para testes de integração

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -4,4 +4,6 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
+	CREATE DATABASE ordering_test;
+	CREATE DATABASE billing_test;
 EOSQL
